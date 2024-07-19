98f125b05ba4e545bf05952020bd6341ad6258ef
MCT32 <32090502+MCT32@users.noreply.github.com>
2024-07-19 13:21:28 +1000

    basic structure

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿class Bank
+{
+    private Tuple<string, string>[] accounts;
+
+    public Bank()
+    {
+        accounts = [
+            new Tuple<string, string>("Joe.Doe", "Password123")
+        ];
+    }
+ 
+    public void Login(string username, string password)
+    {
+
+    }
+
+    public void Signup(string username, string password)
+    {
+
+    }
+}
