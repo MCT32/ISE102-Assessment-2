53688a7e5b672026096da9eea0e1c13aa1608473
MCT32 <ivanhen99@gmail.com>
2024-08-22 15:21:52 +1000

    more stuff

--- obj/Debug/net8.0/ISE102-Assessment-2.AssemblyInfo.cs
+++ obj/Debug/net8.0/ISE102-Assessment-2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ISE102-Assessment-2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2192e089dd9ec6d37fc71103395e1345d7cf501")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75e38bcc14518add394188923dc6d468ed322641")]
 [assembly: System.Reflection.AssemblyProductAttribute("ISE102-Assessment-2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ISE102-Assessment-2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
